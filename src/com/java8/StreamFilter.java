5a4a5e80901537ee2f7c4c5e1c7789ea03e0e844
dhatraksr <dhatraksunny@gmail.com>
2020-06-14 22:02:32 +0530

    v1

---
+++ src/com/java8/StreamFilter.java
@@ -0,0 +1,30 @@
+package com.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamFilter {
+
+	private static List<Integer> list;
+
+	public static void main(String[] args) {
+
+		list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 8, 10);
+
+		/*
+		 * print even numbers
+		 */
+		System.out.println("even numbers are - ");
+		list.stream().filter(num -> num % 2 == 0).forEach(System.out::println);
+
+		/*
+		 * count even numbers
+		 */
+
+		long count = list.stream().filter(num -> num % 2 == 0).count();
+
+		System.out.println("even numbers count " + count);
+
+	}
+
+}
